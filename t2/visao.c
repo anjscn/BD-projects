d91d68a87ec22f80ffeaae85b17b0aa5bf557212
erb19 <erb19@inf.ufpr.br>
2022-09-08 18:32:39 -0300

    finaliza sem verificar atributos

--- t2/visao.c
+++ t2/visao.c
@@ -18,40 +18,140 @@ void troca(transacao_t **e, int l, int i ) {
     e[i] = temp;
 }
 
-// retorna o id da ultima transacao que tem o ultimo write
-// retorna -1 caso não encontre escrita
-int seleciona_ultima_escrita(transacao_t** e, int n){
-    for(int i = n-1; i >=0; i--) {
-        for (int j = e[i]->num_ops-1; j >= 0; j--) {
-            if(e[i]->ops[j]->operation == WRITE){
-                return e[i]->transation_id;
-            }   
+operacoes_t* seleciona_ultima_op_em_t(transacao_t* t, char op){
+    
+    for (int i = t->num_ops-1;  i >= 0; i--) {
+        if(t->ops[i]->operation == op){
+            return t->ops[i];
         }
     }
-    return -1;
+    fprintf(stderr, "Nulo\n");
+    return NULL;
+}
+
+
+// retorna o tempo da ultima transacao que tem o ultimo write
+// retorna -1 caso não encontre escrita
+int seleciona_ultima_op(transacao_t** e, int n, char op, int take_time_in_account){
+    int last_time = -1;
+
+    for(int i = n-1; i >=0; i--) {
+        operacoes_t* ultimo_w = seleciona_ultima_op_em_t(e[i], op);
+        if( (ultimo_w) && ultimo_w->time > last_time){
+            last_time = ultimo_w->time;
+        
+            if(!take_time_in_account){
+                return last_time;
+            }
+        }
+    }
+
+    return last_time;
 }
 
 int valida_ultimo(transacao_t **a, int n){
+    int time_last_w_original = seleciona_ultima_op(original, n, WRITE, 1);
+    int time_last_w_a = seleciona_ultima_op(a, n, WRITE, 0);
 
-    int id_last_w_original = seleciona_ultima_escrita(original, n);
-    int id_last_w_a = seleciona_ultima_escrita(a, n);
+    fprintf(stderr, "original:%d, a:%d \n", time_last_w_original, time_last_w_a);
 
-    fprintf(stderr, "<-original: %d a: %d\n", id_last_w_original, id_last_w_a);
+    if(time_last_w_original == -1){ return 0;}    
 
-    if(id_last_w_original == -1){ return 0;}    
-    if(id_last_w_a != id_last_w_original){ return 0; }
-    fprintf(stderr, "<-GAYYY: %d a: %d\n", id_last_w_original, id_last_w_a);
-
+    if(time_last_w_a != time_last_w_original){ return 0; }
 
     return 1;
 }
 
+operacoes_t* seleciona_primeira_op_em_t(transacao_t* t, char op){
+    
+    for (int i = 0; i < t->num_ops; i++) {
+        if(t->ops[i]->operation == op){
+            return t->ops[i];
+        }
+    }
+    
+    return NULL;
+}
+
+int seleciona_w_previos (transacao_t ** antecessoras, int time, int n) {
+    int qnt = 0;
+    fprintf(stderr, "%d precisa ter ", time);
+    for (int i = 0; i < n; i++) {
+        operacoes_t* primeiro_r = seleciona_primeira_op_em_t(original[i], READ);
+        if( (primeiro_r) && (primeiro_r->time < time) ) {
+            fprintf(stderr, "%d ", original[i]->transation_id);
+            antecessoras[qnt] = original[i];
+            qnt++;
+        }
+    }
+    fprintf(stderr, "ocorrendo antes\n");
+    return qnt;
+}
+
+int remove_da_lista (int id, transacao_t** lista, int tam_lista) {
+    for(int i = 0; i < tam_lista; i++){
+        if(lista[i]->transation_id == id){
+            lista[i] = NULL;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int verifica_previos (transacao_t** lista, int tam_lista, transacao_t** a, int id, int tam_a) {
+    fprintf(stderr, "%d: Ocorrem: ", tam_lista);
+    int faltam = tam_lista;
+    for(int i = 0; i < tam_a; i++) {
+        if(a[i]->transation_id == id){
+            fprintf(stderr, ". Vish, cabou\n");
+            return -1;
+        }
+
+        if(remove_da_lista(a[i]->transation_id, lista, tam_lista) == 1){
+            fprintf(stderr, "%d ", a[i]->transation_id);
+            faltam --;
+        }
+
+        if(faltam == 1){
+            fprintf(stderr, "! Todos\n");
+            return 0;
+        }
+    }
+    fprintf(stderr, ". E só, faltaram %d\n", faltam);
+
+    return -1;
+}
+
+int valida_w_antes_de_r(transacao_t **a, int n){
+
+    // encontra ultimo r em Ti de tempo t
+    // percorre antecessores e anota quem possui write com tempo < t
+    // verifica se de fato vem antes
+    // 
+
+    transacao_t** antecessoras = malloc( (n-1) * sizeof(transacao_t*)); // lista de transações que devem preceder a atual
+
+    operacoes_t* last_r_original;
+    int qnt = 0;
+    for(int i = 0; i < n; i ++){
+        last_r_original = seleciona_ultima_op_em_t(original[i], READ);
+        if(last_r_original){
+            qnt = seleciona_w_previos(antecessoras, last_r_original->time, n);
+            if(verifica_previos(antecessoras, qnt, a, original[i]->transation_id, n) == -1){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+
 int compara(transacao_t **a, int n){
     if(!valida_ultimo(a, n)) return -1;
-
-    for(int i = 0; i < n; i++){
-
-    }
+fprintf(stderr, "É valido 1\n");
+    if(!valida_w_antes_de_r(a, n)) return -1;
+fprintf(stderr, "É valido 2\n");
 
 
     return 0;
@@ -71,20 +171,20 @@ void printa(transacao_t **a, int n){
 
 
 int permuta (transacao_t** e, int l, int r){
-    //fprintf(stderr, "P %d %d\n", l, r);
+    fprintf(stderr, "P %d %d\n", l, r);
 
     if(l == r){
-        // printa(e, r);
-        // printa(original, r);
-        if(compara(e, r) == -1) return -1;
+        printa(e, r);
+        printa(original, r);
+        if(compara(e, r) == 0) return 0;
     }
 
     for(int i = l; i < r; i++){
         troca(e, l, i);
-        if(permuta(e, l+1, r) == -1) return -1;
+        if(permuta(e, l+1, r) == 0) return 0;
         troca(e, l, i);
     }
-    return 0;
+    return -1;
 }
 
 
